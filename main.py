6d5ef118d84a5cebda1715ca5aada54fdd898e95
Raman <149716575+RamanSashyn@users.noreply.github.com>
2024-12-11 17:55:00 +0100

    Сбор структуры данных

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import json
 import requests
-
-
+from geopy import distance
+from pprint import pprint
 
 
 apikey = '60c9f71a-6312-4c7e-91e6-670e028c5937'
@@ -25,6 +25,12 @@ def fetch_coordinates(apikey, address):
     return lon, lat
 
 
+def calculate_distance(lat1, lon1, lat2, lon2):
+    coords_1 = (lat1, lon1)
+    coords_2 = (lat2, lon2)
+    return distance.distance(coords_1, coords_2).km
+
+
 location = input('Где вы находитесь? ')
 coords = fetch_coordinates(apikey, location)
 print(f'Ваши координаты: {coords}')
@@ -34,8 +40,42 @@ with open("coffee.json", "r", encoding='CP1251') as coffee_file:
     file_content = coffee_file.read()
     coffee_list = json.loads(file_content)
 
+cafes_info = []
+
 for coffee in coffee_list:
-    print(coffee["Name"], coffee['Latitude_WGS84'], coffee['Longitude_WGS84'])
+    coffee_name = coffee["Name"]
+    coffee_longitude = coffee["geoData"]["coordinates"][0]
+    coffee_latitude = coffee["geoData"]["coordinates"][1]
+    distance_to_cafe = calculate_distance(
+        coords[1], coords[0], coffee_latitude, coffee_longitude
+    )
+
+
+    cafes_info.append({
+        'title': coffee_name,
+        'distance': distance_to_cafe,
+        'latitude': coffee_latitude,
+        'longitude': coffee_longitude,
+    })
+
+
+pprint(cafes_info)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
