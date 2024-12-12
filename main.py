de1982763bd4adc5456da2b1d57d6c849ce2c376
Raman <149716575+RamanSashyn@users.noreply.github.com>
2024-12-12 16:18:00 +0100

    Создаем карту с местоположением

--- main.py
+++ main.py
@@ -1,28 +1,32 @@
 import json
 import requests
+import folium
+from dotenv import load_dotenv
 from geopy import distance
 from pprint import pprint
-
-
-
-apikey = '60c9f71a-6312-4c7e-91e6-670e028c5937'
+import os
 
 
 def fetch_coordinates(apikey, address):
     base_url = "https://geocode-maps.yandex.ru/1.x"
-    response = requests.get(base_url, params={
-        "geocode": address,
-        "apikey": apikey,
-        "format": "json",
-    })
+    response = requests.get(
+        base_url,
+        params={
+            "geocode": address,
+            "apikey": apikey,
+            "format": "json",
+        },
+    )
     response.raise_for_status()
-    found_places = response.json()['response']['GeoObjectCollection']['featureMember']
+    found_places = response.json()["response"]["GeoObjectCollection"][
+        "featureMember"
+    ]
 
     if not found_places:
         return None
 
     most_relevant = found_places[0]
-    lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
+    lon, lat = most_relevant["GeoObject"]["Point"]["pos"].split(" ")
     return lon, lat
 
 
@@ -32,60 +36,64 @@ def calculate_distance(lat1, lon1, lat2, lon2):
     return distance.distance(coords_1, coords_2).km
 
 
-location = input('Где вы находитесь? ')
-coords = fetch_coordinates(apikey, location)
-print(f'Ваши координаты: {coords}')
-
-
-with open("coffee.json", "r", encoding='CP1251') as coffee_file:
-    file_content = coffee_file.read()
-    coffee_list = json.loads(file_content)
-
-cafes_info = []
-
-
-for coffee in coffee_list:
-    coffee_name = coffee["Name"]
-    coffee_longitude = coffee["geoData"]["coordinates"][0]
-    coffee_latitude = coffee["geoData"]["coordinates"][1]
-    distance_to_cafe = calculate_distance(
-        coords[1], coords[0], coffee_latitude, coffee_longitude
-    )
-
-
-    cafes_info.append({
-        'title': coffee_name,
-        'distance': distance_to_cafe,
-        'latitude': coffee_latitude,
-        'longitude': coffee_longitude
-    })
-
-
 def get_cafe_distance(cafe):
     return cafe["distance"]
 
 
+def main():
+    load_dotenv()
+    apikey = os.getenv("API_KEY")
 
-nearby_cafes = sorted(cafes_info, key=get_cafe_distance)[:5]
-pprint(nearby_cafes, sort_dicts=False)
+    location = input("Где вы находитесь? ")
+    coords = fetch_coordinates(apikey, location)
+    if not coords:
+        print("Проверьте адрес!")
+        return
+    print(f"Ваши координаты: {coords}")
+
+    with open("coffee.json", "r", encoding="CP1251") as coffee_file:
+        file_content = coffee_file.read()
+        coffee_list = json.loads(file_content)
+
+    cafes_info = []
+
+    for coffee in coffee_list:
+        coffee_name = coffee["Name"]
+        coffee_longitude = coffee["geoData"]["coordinates"][0]
+        coffee_latitude = coffee["geoData"]["coordinates"][1]
+
+        distance_to_cafe = calculate_distance(
+            coords[1], coords[0], coffee_latitude, coffee_longitude
+        )
+        cafes_info.append(
+            {
+                "title": coffee_name,
+                "distance": distance_to_cafe,
+                "latitude": coffee_latitude,
+                "longitude": coffee_longitude,
+            }
+        )
+
+    nearby_cafes = sorted(cafes_info, key=get_cafe_distance)[:5]
+    pprint(nearby_cafes, sort_dicts=False)
+
+    map = folium.Map(location=[coords[1], coords[0]], zoom_start=11)
+
+    folium.Marker(
+        location=[coords[1], coords[0]],
+        popup="Вы здесь",
+        icon=folium.Icon(color="red"),
+    ).add_to(map)
+
+    for cafe in nearby_cafes:
+        folium.Marker(
+            location=[cafe["latitude"], cafe["longitude"]],
+            popup=f"{cafe['title']} ({cafe['distance']:.2f} км)",
+            icon=folium.Icon(color="green"),
+        ).add_to(map)
+
+    map.save("index.html")
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == "__main__":
+    main()
